f57ceb8f6964d227bae3abeea04fb74dbf9f65aa
alhabib5565 <alhabib5565@gmail.com>
2025-05-07 20:38:12 +0600

    learn about pointer

---
+++ introduction-to-programming/module-15/pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x = 10;
+    printf("%d\n", x);
+    printf("%p\n", &x);
+
+    int *p = &x;
+    printf("%d\n", *p);
+
+    *p = 20;
+    printf("%d", x);
+    return 0;
+};
