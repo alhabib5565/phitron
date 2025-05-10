080d1ef68cb49eb84390cb57516efbc557cbe384
alhabib5565 <alhabib5565@gmail.com>
2025-05-10 22:48:03 +0600

    "Stones on the Table" problem is solved

---
+++ introduction-to-programming/my-extra-practice/A_Stones_on_the_Table.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    char str[n];
+    scanf("%s", str);
+    int count = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (str[i] == str[i + 1])
+        {
+            count++;
+        }
+    }
+    printf("%d", count);
+    return 0;
+};
